dd543a7e5625509fdd98f9502808eade25cc4bb8
srd373 <78557758+srd373@users.noreply.github.com>
2021-02-17 17:59:59 -0800

    Add files via upload

--- solution.py
+++ solution.py
@@ -16,10 +16,13 @@ def webServer(port=13331):
         print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept()#Fill in start     #Fill in end
         try:
-            message = "Connection established!"#Fill in start    #Fill in end
+            message = connectionSocket.recv(1024)#"Connection established!"#Fill in start    #Fill in end
             filename = message.split()[1]
             f = open(filename[1:])
-            outputdata = "helloworld.html"#Fill in start     #Fill in end
+            file = open("helloworld.html", 'rb')
+            outputdata = str(file.read())[2:-1]#"helloworld.html"#Fill in start     #Fill in end
+            print(outputdata)
+            file.close()
 
             #Send one HTTP header line into socket
             #Fill in start
